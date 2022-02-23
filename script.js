a5321d0722113a93b3236fdbf720a1654242fc5f
John <dab.cena420@gmail.com>
2022-02-22 17:39:15 -0800

    Built out computerPlay function

---
+++ script.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+    let options = ["rock", "paper", "scissors"];
+    let choice = options[Math.floor(Math.random() * options.length)];
+    return choice; //"rock", "paper", or "scissors"
+    //console.log(choice);
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+}
