60970e347a5ee154ea85d5e4b9540f342efcb50c
John <dab.cena420@gmail.com>
2022-02-22 17:50:09 -0800

    Added psuedocode and switch logic structure

--- script.js
+++ script.js
@@ -6,5 +6,41 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    /* psuedocode
+    change player input to case-sensitive 
+    player plays rock
+        computer plays rock 
+            tie
+        computer plays paper
+            lose 
+        computer plays scissors 
+            win
+    player plays paper
+        computer plays rock
+            win 
+        computer plays paper
+            tie 
+        computer plays scissors
+            lose 
+    player plays scissors
+        computer plays rock
+            lose 
+        computer plays paper
+            win 
+        computer plays scissors
+            tie 
+    player plays none of the options
+        fail
+    */
+
+    switch(x) {
+        case playerChoseRock:
+            break;
+        case playerChosePaper:
+            break;
+        case playerChoseScissors:
+            break;
+        case playerChoseNone:
+            break;
+    }
 }
